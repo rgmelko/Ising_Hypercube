fb74d6b945afc5470854dc28445a648b8f7a8c33
Roger Melko <rgmelko@v1020-wn-147-30.campus-dynamic.uwaterloo.ca>
2016-09-28 11:27:08 -0400

    Random initialization of spin - works for any d

--- generalD_1_2.code.h
+++ generalD_1_2.code.h
@@ -77,11 +77,12 @@ GeneralD12Code::GeneralD12Code(Spins & sigma, const HyperCube & cube, const doub
     N1 = D_*cube.N_; //number of 1 cells
 
     sigma.resize(N1); //these are the degrees of freedom (1 cells)
-    //sigma.randomize();
-    for (int i=0; i<L_*L_; i++) {   //Wz = -1
-        //sigma.flip(D_*i+0); 
-        sigma.flip(D_*i+2); 
-    }
+    sigma.randomize();
+    // Below works for 3D only
+    //for (int i=0; i<L_*L_; i++) {   //Wz = -1
+    //    //sigma.flip(D_*i+0); 
+    //    sigma.flip(D_*i+2); 
+    //}
 
     //use it to built the sigma-z plaquettes
     vector <int> temp;
